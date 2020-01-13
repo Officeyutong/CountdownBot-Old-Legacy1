fb7ffeb2bdb20be890fc2caae894c97b92ee79a7
officeyutong <i@ytbolg.net>
2020-01-13 12:41:17 +0800

    sympy封装

--- plugins/math/math.py
+++ plugins/math/math.py
@@ -10,6 +10,7 @@ import time
 import base64
 from io import BytesIO
 import global_vars
+from typing import List
 config = global_vars.CONFIG[__name__]
 
 
@@ -17,7 +18,7 @@ def plugin():
     return {
         "author": "officeyutong",
         "version": 1.0,
-        "description": "数学积分和Latex渲染支持."
+        "description": "sympy功能封装"
     }
 
 
@@ -56,7 +57,7 @@ def integrate(bot: CQHttp, context=None, args=None):
 
 
 @command(name="latex", help="渲染Latex公式")
-def renderlatex(bot: CQHttp, context=None, args=None)->None:
+def renderlatex(bot: CQHttp, context=None, args=None) -> None:
     from io import BytesIO
     import base64
     formula = "".join(map(lambda x: x+" ", args[1:]))
@@ -70,7 +71,77 @@ def renderlatex(bot: CQHttp, context=None, args=None)->None:
         bot.send(context, "渲染Latex时发生错误:\n{}".format(ex))
 
 
-def renderLatex(formula: str)->BytesIO:
+@command(name="diff", help="求导")
+def differentiate(bot: CQHttp, context: dict, args: List[str]) -> None:
+
+    def process():
+        func = "".join(map(lambda x: x+" ", args[1:]))
+        x = sympy.symbols("x")
+        print_log("diff for "+func)
+
+        def diff():
+            print_log("Starting...")
+            try:
+                res = sympy.diff(func, x)
+                buffer: BytesIO = renderLatex(
+                    "$${}$$".format(sympy.latex(res)))
+                bot.send(context, "Python表达式:\n{}\n\nLatex:\n{}\n\n图像:\n[CQ:image,file=base64://{}]".format(
+                    res, sympy.latex(res), base64.encodebytes(buffer.getvalue()).decode(
+                        "utf-8").replace("\n", "")))
+            except Exception as ex:
+                bot.send(context, ("{}".format(ex))[:300])
+                raise ex
+            print_log("Done...")
+        thd2 = threading.Thread(target=diff)
+        thd2.start()
+        begin = time.time()
+        while time.time()-begin < 5:
+            time.sleep(0.1)
+        if thd2.is_alive():
+            bot.send(context, "求导{}运行超时.".format(func))
+            util.stop_thread(thd2)
+
+    thread = threading.Thread(target=process)
+    thread.start()
+
+
+@command(name="series", help="级数展开 series 展开点 函数")
+def series(bot: CQHttp, context: dict, args: List[str]) -> None:
+
+    def process():
+        x0 = args[1]
+        func = "".join(map(lambda x: x+" ", args[2:]))
+        x = sympy.symbols("x")
+        print_log("series for "+func)
+
+        def series():
+            print_log("Starting...")
+            try:
+                res = sympy.series(func, x0=sympy.simplify(x0), n=10, x=x)
+                buffer: BytesIO = renderLatex(
+                    "$${}$$".format(sympy.latex(res)))
+                print("sending...")
+                bot.send(context, "Python表达式:\n{}\n\nLatex:\n{}\n\n图像:\n[CQ:image,file=base64://{}]".format(
+                    res, sympy.latex(res), base64.encodebytes(buffer.getvalue()).decode(
+                        "utf-8").replace("\n", "")))
+            except Exception as ex:
+                bot.send(context, ("{}".format(ex))[:300])
+                raise ex
+            print_log("Done...")
+        thd2 = threading.Thread(target=series)
+        thd2.start()
+        begin = time.time()
+        while time.time()-begin < 5:
+            time.sleep(0.1)
+        if thd2.is_alive():
+            bot.send(context, "级数{}运行超时.".format(func))
+            util.stop_thread(thd2)
+
+    thread = threading.Thread(target=process)
+    thread.start()
+
+
+def renderLatex(formula: str) -> BytesIO:
     from sympy import preview
     print_log("Rendering {}".format(formula))
     buffer = BytesIO()
